291785e3584d0a997fa1d050f650e9a0895bda7e
Matt Reid <hello@mattreid.dev>
2019-12-12 15:31:40 +0000

    Created basic contact page and updated home.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/', to: 'home#home'
+  get 'contact', to: 'home#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
