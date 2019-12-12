405d197a88693a540dc9646d0c3ac9b2bde1d8e3
Matt Reid <hello@mattreid.dev>
2019-12-12 16:23:08 +0000

    Bug fixes and added tests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/', to: 'home#home'
+  root 'home#home'
+  #get '/', to: 'home#home'
   get 'contact', to: 'home#contact'
   post 'request_contact', to: 'home#request_contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,12 +2,26 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-	get '/'
-	assert_response :success
+    get '/'
+    assert_response :success
   end
 
   test 'should get contact' do
-	get contact_url
-	assert_response :success
+    get contact_url
+    assert_response :success
+  end
+
+  test "should post request contact but no email" do
+    post request_contact_url
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+
+  test "should post request contact" do post request_contact_url, params:
+    {name: "Matthew", email: "mr00846@surrey.ac.uk", telephone: "1234567890", message: "Hello"}
+    assert_response :redirect
+    assert_nil flash[:alert]
+    assert_not_empty flash[:notice]
   end
 end
