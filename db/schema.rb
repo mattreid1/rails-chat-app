a429eb66159c9066a03f0aee87b38c8bd1a5fbea
Matt Reid <hello@mattreid.dev>
2019-12-15 02:32:01 +0000

    Added Post and checks if signed in else Anonymous.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_012250) do
+ActiveRecord::Schema.define(version: 2019_12_15_021729) do
 
   create_table "messages", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2019_12_15_012250) do
     t.integer "to_id"
     t.index ["to_id"], name: "index_messages_on_to_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
