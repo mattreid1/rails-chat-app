7a38a68ae4d248f66d577a08042d57f3c7ba4d45
Matt Reid <hello@mattreid.dev>
2019-12-16 14:23:14 +0000

    Added tests and fixed validation.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,6 @@ class Profile < ApplicationRecord
 
   validates :user_id, presence: true, uniqueness: true, numericality: true
   validates :name, presence: true, length: { maximum: 40 }
-  validates :student, presence: true
 
   scope :profile_info, ->(user) {where(["user_id = ?", user.id])}
 end
--- test/controllers/chatroom_controller_test.rb
+++ test/controllers/chatroom_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ChatroomControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get "/chatroom"
+    assert_response :success
+  end
 end
